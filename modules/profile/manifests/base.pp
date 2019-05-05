307d7f485fa08b9e7fb2f11c3a8f3511521b7e4f
Guy Newbury <guyn@live.co.uk>
2019-05-05 16:01:02 +0100

    Add a third test file

--- modules/profile/manifests/base.pp
+++ modules/profile/manifests/base.pp
@@ -13,6 +13,11 @@ class profile::base (
 		path 	=> '/puppet2.info',
 		content => 'Hello',
 	  }
+	  
+	  file { 'info-three':
+		path 	=> '/puppet3.info',
+		content => 'This is boring now',
+	  }
     }
     'Windows' : {
 		include profile::base::windows
