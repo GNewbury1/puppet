65c46011f6191f87b63e3368a64557bd2eb82cd4
Guy Newbury <guyn@live.co.uk>
2019-06-21 21:26:03 +0100

    Add javahome

--- modules/profile/manifests/stash.pp
+++ modules/profile/manifests/stash.pp
@@ -4,15 +4,17 @@ class profile::stash {
   $db_host = lookup(profile::mysql::host)
 
   class { 'stash':
-    version => '6.4',
-    jvm_xms => '512M',
-    jvm_xmx => '512M',
-    dburl => $db_host,
+    version    => '6.4',
+    jvm_xms    => '512M',
+    jvm_xmx    => '512M',
+    dburl      => $db_host,
     dbpassword => $db_pass,
+    javahome   => '/opt/java'
   }
 
   class { 'java':
     distribution => 'jre',
+
   }
 
 }
