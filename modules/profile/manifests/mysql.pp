8455bd503a44c05a45de204ecd0fce9ab03bd7a8
Guy Newbury <guyn@live.co.uk>
2019-06-29 13:59:51 +0100

    Syntax

--- modules/profile/manifests/mysql.pp
+++ modules/profile/manifests/mysql.pp
@@ -1,17 +1,35 @@
 class profile::mysql {
 
-  include ::mysql::server
 
   $db_name = lookup(profile::mysql::name)
   $db_user = lookup(profile::mysql::user)
   $db_pass = lookup(profile::mysql::pass)
   $db_host = lookup(profile::mysql::host)
   $db_perm = lookup(profile::mysql::perm)
+  $db_fulluser = lookup(profile::mysql::fulluser)
 
-  mysql::db { $db_name:
-    user     => $db_user,
-    password => $db_pass,
-    host     => $db_host,
-    grant    => $db_perm,
+  class { '::mysql::server':
+    databases => {
+      "${db_name}" => {
+        ensure  => 'present',
+        charset => 'utf8',
+      },
+    },
+
+    users => {
+      $db_fulluser => {
+        ensure        => 'present',
+        password_hash => $db_pass
+      },
+    },
+
+    grants => {
+      "${db_fulluser}/${db_name}.*" => {
+        ensure     => 'present',
+        privileges => $db_perm,
+        table      => "${db_name}.*",
+        user       => $db_fulluser
+      }
+    }
   }
 }
